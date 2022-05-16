76ddd852d7720c627d02be7a2202e8f2617ea13e
AlphaGoat <pjtpeterjohnthomas@gmail.com>
2022-05-16 10:10:42 -0400

    Debugging with print statements

--- CORONAnet/train.py
+++ CORONAnet/train.py
@@ -116,9 +116,6 @@ def train(
         # begin training loop
         for step, (images, targets) in enumerate(train_dataset):
 
-            print("images: ", images)
-            tf.print("images: ", tf.shape(images))
-
             with tf.GradientTape() as tape:
                 pred_results = model(images, training=True)
 
